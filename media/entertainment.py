7f4c0e439cab552a4d80469684297bb899758ad4
matimartin <matias.martin89@gmail.com>
2017-10-10 18:18:29 -0300

    Commit of the first project of the nanodegree full stack

---
+++ media/entertainment.py
@@ -0,0 +1,16 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toy that come to life",
+                        "https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print (toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                     "A marin on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg"
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+print (avatar.storyline)
+
